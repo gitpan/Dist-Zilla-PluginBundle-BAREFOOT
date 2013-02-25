f73e466ad410319735ce15789aabae847415ea0c
Buddy Burden <barefoot@cpan.org>
2013-02-25 10:09:05 +0000

    Import of BAREFOOT/Dist-Zilla-PluginBundle-BAREFOOT-0.03_01 from CPAN.
    gitpan-cpan-distribution: Dist-Zilla-PluginBundle-BAREFOOT
    gitpan-cpan-version:      0.03_01
    gitpan-cpan-path:         BAREFOOT/Dist-Zilla-PluginBundle-BAREFOOT-0.03_01.tar.gz
    gitpan-cpan-author:       BAREFOOT
    gitpan-cpan-maturity:     developer

--- lib/Pod/Weaver/PluginBundle/BAREFOOT.pm
+++ lib/Pod/Weaver/PluginBundle/BAREFOOT.pm
@@ -13,7 +13,7 @@ use warnings;
 	use Pod::Weaver::Section::Support 1.001 ();
 
 
-	our $VERSION = '0.03'; # VERSION
+	our $VERSION = '0.03_01'; # VERSION
 
 	my $bugtracker_content = <<'END';
 		This module is on GitHub.  Feel free to fork and submit patches.  Please note that I develop
@@ -90,7 +90,7 @@ Pod::Weaver::PluginBundle::BAREFOOT - BAREFOOT's default Pod::Weaver config
 
 =head1 VERSION
 
-version 0.03
+version 0.03_01
 
 =head1 DESCRIPTION
 
--- lib/Pod/Weaver/Section/LegalWithAddendum.pm
+++ lib/Pod/Weaver/Section/LegalWithAddendum.pm
@@ -5,7 +5,7 @@ use Method::Signatures::Modifiers;
 class Pod::Weaver::Section::LegalWithAddendum extends Pod::Weaver::Section::Legal
 {
 
-	our $VERSION = '0.03'; # VERSION
+	our $VERSION = '0.03_01'; # VERSION
 
 
 	has addendum => ( is => 'ro', isa => 'Str', predicate => '_has_addendum' );
@@ -35,7 +35,7 @@ Pod::Weaver::Section::LegalWithAddendum - Dist::Zilla configuration the way BARE
 
 =head1 VERSION
 
-version 0.03
+version 0.03_01
 
 =head1 AUTHOR
 
